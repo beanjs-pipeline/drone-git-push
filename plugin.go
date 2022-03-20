2e3dcede7ce8016956ecc447173d9520a3b6cf66
beanjs <445212619@qq.com>
2022-03-21 00:12:54 +0800

    fix

--- plugin.go
+++ plugin.go
@@ -29,6 +29,7 @@ func (p *Plugin) run(c *exec.Cmd) error {
 	}
 
 	cacheDir := path.Join(cwd, p.LocalDir)
+	log.Printf("cache dir: %s", cacheDir)
 
 	c.Env = os.Environ()
 	c.Stderr = os.Stderr
@@ -43,19 +44,27 @@ func (p *Plugin) Exec() error {
 		return err
 	}
 
-	// gitAdd := exec.Command("git", "add", ".")
-	// gitCommit := exec.Command("git", "commit", "-a", "-m", p.CommitMessage)
+	log.Printf("git config")
+	if err := p.run(exec.Command("git", "config", "--global", "user.email", "drone-ci@gmail.com")); err != nil {
+		return err
+	}
+	if err := p.run(exec.Command("git", "config", "--global", "user.name", "drone-ci")); err != nil {
+		return err
+	}
+
+	gitAdd := exec.Command("git", "add", ".")
+	gitCommit := exec.Command("git", "commit", "-a", "-m", p.CommitMessage)
 	gitPush := exec.Command("git", "push", "-u", "origin", p.Branch)
 
-	// log.Println("Add file contents to the index")
-	// if err := p.run(gitAdd); err != nil {
-	// 	return err
-	// }
+	log.Println("Add file contents to the index")
+	if err := p.run(gitAdd); err != nil {
+		return err
+	}
 
-	// log.Println("Record changes to the repository")
-	// if err := p.run(gitCommit); err != nil {
-	// 	return err
-	// }
+	log.Println("Record changes to the repository")
+	if err := p.run(gitCommit); err != nil {
+		return err
+	}
 
 	log.Println("Update remote refs along with associated objects")
 	if err := p.run(gitPush); err != nil {
